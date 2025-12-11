d0ec139e1739698f18f069746c98c53f9ebbb40e
iftekhs <iftekhstudio@gmail.com>
2025-12-11 20:27:32 +0600

    about page customization complete

--- database/seeders/CustomizationSeeder.php
+++ database/seeders/CustomizationSeeder.php
@@ -118,8 +118,92 @@ class CustomizationSeeder extends Seeder
         ]);
 
         Customization::create([
+            'key' => 'about_page',
+            'label' => 'About Page',
+            'fields' => [
+                [
+                    'key' => 'title',
+                    'label' => 'Title',
+                    'type' => 'text',
+                    'rules' => ['nullable', 'string', 'max:100'],
+                ],
+                [
+                    'key' => 'banner',
+                    'label' => 'Banner (1900 x 560 px)',
+                    'type' => 'image',
+                    'rules' => ['nullable', 'image', 'max:2048'],
+                ],
+                [
+                    'key' => 'images',
+                    'label' => 'Images',
+                    'type' => 'array',
+                    'rules' => ['array'],
+                    'items' => [
+                        [
+                            'key' => 'file',
+                            'label' => 'File (350 x 320 px)',
+                            'type' => 'image',
+                            'rules' => ['nullable', 'image', 'max:2048'],
+                        ],
+                    ]
+                ],
+                [
+                    'key' => 'content',
+                    'label' => 'Content',
+                    'type' => 'text',
+                    'rules' => ['nullable', 'string', 'max:2048'],
+                ],
+                [
+                    'key' => 'block_title',
+                    'label' => 'Block Title',
+                    'type' => 'text',
+                    'rules' => ['nullable', 'string', 'max:100'],
+                ],
+                [
+                    'key' => 'block_content',
+                    'label' => 'Block Content',
+                    'type' => 'text',
+                    'rules' => ['nullable', 'string', 'max:1024'],
+                ],
+            ],
+            'value' => [
+                'title' => 'OUR INSPIRING JOURNEY',
+                'banner' => null,
+                'images' => [],
+                'content' => `Mlouye’s founder and creative director Meb Rure hails from an
+                                industrial design background. In 2015, Meb decided to change gears
+                                and turn her energy towards Mlouye, a collection of exceptional
+                                handbags. Focusing on quality material, good design, craftsmanship
+                                and sustainability, Mlouye reflects the epitome of quality over
+                                quantity.
+                                
+                                Meb was highly inspired by the Bauhaus Movement's artists and
+                                architects. From Mies van der Rohe's works to Kandinsky's
+                                paintings, to Aalto's furniture, she acquired a rationalist vision
+                                of design by gleaning how they served a utilitarian purpose in a
+                                cleverly simple way. Mlouye merges industrial design and fashion,
+                                creating functional handbags made of luxurious and honest
+                                materials to improve people's lives in small but important ways.
+                                
+                                
+                                Innovation being the key factor alongside aesthetic, Mlouye has
+                                brought unexpected shapes with smart details, functionality and a
+                                new luxury feel with a contemporary price point.
+                                
+                                Istanbul is where Mlouye was born, the company's headquarters is
+                                now split between the US, and Turkey.`,
+                'block_title' => 'Our quality promise',
+                'block_content' => `Quality is never an accident. It is always the result of intelligent
+                            effort. We spend most of our time and energy for good design
+                            and to achieve high quality. Every single detail from material to
+                            technique is thought through  with obsessive attention. If our
+                            product doesn't satisfy you, we'll take it back.`,
+            ],
+        ]);
+
+        Customization::create([
             'key' => 'footer',
-            'label' => 'Footer',
+            'label' => 'Footer Component',
             'fields' => [
                 [
                     'key' => 'about',
@@ -127,9 +211,53 @@ class CustomizationSeeder extends Seeder
                     'type' => 'text',
                     'rules' => ['nullable', 'string', 'max:1024'],
                 ],
+                [
+                    'key' => 'copyright',
+                    'label' => 'Copyright',
+                    'type' => 'text',
+                    'rules' => ['nullable', 'string', 'max:255'],
+                ],
+                [
+                    'key' => 'social_links',
+                    'label' => 'Social Links',
+                    'type' => 'array',
+                    'rules' => ['array'],
+                    'items' => [
+                        [
+                            'key' => 'icon',
+                            'label' => 'Icon',
+                            'type' => 'text',
+                            'rules' => ['required', 'string', 'max:50'],
+                        ],
+                        [
+                            'key' => 'url',
+                            'label' => 'URL',
+                            'type' => 'text',
+                            'rules' => ['required', 'string', 'url', 'max:1024'],
+                        ],
+                    ]
+                ],
             ],
             'value' => [
-                'about' => '',
+                'about' => null,
+                'copyright' => '© 2025 - ArushiMart - Bangladesh',
+                'social_links' => []
+            ],
+        ]);
+
+        Customization::create([
+            'key' => 'auth',
+            'label' => 'Authentication Page',
+            'fields' => [
+                [
+                    'key' => 'background_image',
+                    'label' => 'Background Image',
+                    'type' => 'image',
+                    'rules' => ['nullable', 'image', 'max:2048'],
+                ],
+            ],
+            'value' => [
+                'background_image' => null,
             ],
         ]);
     }
--- database/seeders/ProductionSeeder.php
+++ database/seeders/ProductionSeeder.php
@@ -8,17 +8,13 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ProductionSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Superadmin',
             'email' => 'arushimart0@gmail.com',
@@ -26,17 +22,7 @@ class DatabaseSeeder extends Seeder
             'status' => UserStatus::ACTIVE,
         ]);
 
-        User::factory()->create([
-            'name' => 'John Doe',
-            'email' => 'user@email.com',
-            'role' => UserRole::USER,
-            'status' => UserStatus::ACTIVE,
-        ]);
-
         $this->call([
-            UserSeeder::class,
-            ProductSeeder::class,
-            OrderSeeder::class,
             CustomizationSeeder::class,
         ]);
     }
