c0d69bbb0e2cd0d9a4ec6d6aca36804a8e5c3880
arushimart <arushimart0@gmail.com>
2025-12-18 13:26:26 +0600

    allowed all origins

--- config/cors.php
+++ config/cors.php
@@ -29,7 +29,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_map('trim', array_filter(explode(',', env('ALLOWED_ORIGINS', '')))),
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
